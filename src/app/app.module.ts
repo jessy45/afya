a3fe3bc5b78504c31ea3b3d07fc317597da41b10
Gloire KAF <gloirekaf.mwansha@gmail.com>
2020-02-22 14:30:16 +0200

    Adding login and signup base component and these service methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { AddProfileComponent } from './add-profile/add-profile.component';
 import { AllProfilesComponent } from './all-profiles/all-profiles.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProfileComponent,
-    AllProfilesComponent
+    AllProfilesComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
